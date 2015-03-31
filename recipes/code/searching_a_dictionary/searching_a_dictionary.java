15a47e1f91747ff369b08eddcebcfae6cfc5f1b3
Cesar Torres <cearto@berkeley.edu>
2015-03-31 10:01:45 -0700

    Remerging

--- recipes/code/searching_a_dictionary/searching_a_dictionary.java
+++ recipes/code/searching_a_dictionary/searching_a_dictionary.java
@@ -1,10 +1,7 @@
 import Helper.txt;
 
-<<<<<<< HEAD
-=======
 public class searching_a_dictionary {
 	public static Array main(string[] args) {}
->>>>>>> 1cb5c07be6b7f11c3bad90bc6fa1ac1976edabc2
 
 Scanner sc = new Scanner(new File(Helper.txt));
 List<String> lines = new ArrayList<String>();
@@ -15,7 +12,7 @@ while (sc.hasNextLine()) {
 String[] textarray = lines.toArray(new String[0]);
 
 
-public static Array getAllIndexes(arr, val) {
+function getAllIndexes(arr, val) {
     var indexes = [], i = -1;
     while ((i = arr.indexOf(val, i+1)) != -1){
         indexes.push(i);
